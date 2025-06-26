0e9df7fba84d78301eb89ea033749c2fe99c7ac3
Robert076 <robertberes06@gmail.com>
2025-06-26 15:11:39 +0300

    some tests for lint command

--- internal/command/lint_command/lint_command_test.go
+++ internal/command/lint_command/lint_command_test.go
@@ -2,6 +2,24 @@ package lint_command
 
 import "testing"
 
-func TestRunLintCommand(t *testing.T) {
+func TestLintCommand_Name(t *testing.T) {
+	cmd := &LintCommand{}
+	if got := cmd.Name(); got != "lint" {
+		t.Errorf("LintCommand.Name() = %v, want %v", got, "lint")
+	}
+}
 
+func TestLintCommand_Description(t *testing.T) {
+	cmd := &LintCommand{}
+	if got := cmd.Description(); got != "Returns linters for supported languages" {
+		t.Errorf("LintCommand.Description() = %v, want %v", got, "Returns linters for supported languages")
+	}
+}
+
+func TestLintCommand_Subcommands(t *testing.T) {
+	cmd := &LintCommand{}
+	subs := cmd.Subcommands()
+	if len(subs) != 0 {
+		t.Errorf("Expected empty subcommands list, got %d", len(subs))
+	}
 }
