98e06121aebbbeec703f8ae3ece6d358f46cf884
Robert076 <robertberes06@gmail.com>
2025-06-26 15:41:16 +0300

    builder pattern for constructing mock commands and some more tests (76.8% coverage so far)

--- mocks/mock_command/mock_command.go
+++ mocks/mock_command/mock_command.go
@@ -19,3 +19,32 @@ func (m *MockCommand) Run(args []string) error {
 	return m.expectedErr
 }
 func (m *MockCommand) Subcommands() []command.Command { return m.subcommands }
+
+type MockCommandBuilder struct {
+	mock *MockCommand
+}
+
+func NewMockCommandBuilder() *MockCommandBuilder {
+	return &MockCommandBuilder{
+		mock: &MockCommand{},
+	}
+}
+
+func (b *MockCommandBuilder) WithName(name string) *MockCommandBuilder {
+	b.mock.name = name
+	return b
+}
+
+func (b *MockCommandBuilder) WithDescription(description string) *MockCommandBuilder {
+	b.mock.description = description
+	return b
+}
+
+func (b *MockCommandBuilder) WithSubcommands(subcommands []command.Command) *MockCommandBuilder {
+	b.mock.subcommands = subcommands
+	return b
+}
+
+func (b *MockCommandBuilder) Build() *MockCommand {
+	return b.mock
+}
