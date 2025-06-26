f6648a1fe1686f552081e7a2de89cec7b897cd65
Robert076 <robertberes06@gmail.com>
2025-06-26 14:31:21 +0300

    lint command looks fine

--- internal/command/lint_command/lint_command.go
+++ internal/command/lint_command/lint_command.go
@@ -1,6 +1,10 @@
 package lint_command
 
-import "github.com/Robert076/devops-buddy/internal/command"
+import (
+	"fmt"
+
+	"github.com/Robert076/devops-buddy/internal/command"
+)
 
 type LintCommand struct{}
 
@@ -13,7 +17,22 @@ func (l *LintCommand) Description() string {
 }
 
 func (l *LintCommand) Run(args []string) error {
-	return nil
+	if len(args) == 0 {
+		fmt.Println("Usage: do lint <language>")
+		fmt.Println("Supported linters:")
+		for _, sub := range l.Subcommands() {
+			fmt.Printf("  %s - %s\n", sub.Name(), sub.Description())
+		}
+		return nil
+	}
+
+	for _, sub := range l.Subcommands() {
+		if sub.Name() == args[0] {
+			return sub.Run(args[1:])
+		}
+	}
+
+	return fmt.Errorf("unknown linter: %s", args[0])
 }
 
 func (l *LintCommand) Subcommands() []command.Command {
