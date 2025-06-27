c77c469034ab08ec383288e034d3829c7bd2acdf
Robert076 <robertberes06@gmail.com>
2025-06-27 21:33:16 +0300

    some tests

--- internal/command/gitignore_command/gitignore_command_test.go
+++ internal/command/gitignore_command/gitignore_command_test.go
@@ -19,12 +19,48 @@ func TestGitignoreCommand_Description(t *testing.T) {
 }
 
 func TestGitignoreCommand_Run(t *testing.T) {
-	tests := []struct {
-		name string
+	integrationTests := []struct {
+		name        string
+		args        []string
+		expectErr   bool
+		errMsg      string
+		description string
+	}{
+		{
+			"Valid - integration test, tests external endpoint",
+			[]string{"go"},
+			false,
+			"",
+			"User requests a gitignore for go by running 'gitignore go', this is an integration test it is not mocked",
+		},
+		{
+			"Invalid - wrong argument given",
+			[]string{"this-gitignore-template-doesnt-exist"},
+			true,
+			"unknown gitignore template: this-gitignore-template-doesnt-exist",
+			"User requests a gitignore template that doesn't exist",
+		},
+	}
 
-		args    []string
-		wantErr bool
-	}{}
+	for _, tt := range integrationTests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := &GitignoreCommand{}
+			err := cmd.Run(tt.args)
+
+			if tt.expectErr {
+				if err == nil {
+					t.Fatalf("Expected error, got nil")
+				}
+				if err.Error() != tt.errMsg {
+					t.Fatalf("Unexpected error: got %s, wanted %s", err, tt.errMsg)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Expected no error, got '%v'", err)
+				}
+			}
+		})
+	}
 }
 func TestGitignoreCommand_Subcommands(t *testing.T) {
 	cmd := &GitignoreCommand{}
