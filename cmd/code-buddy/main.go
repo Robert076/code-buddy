f9f29d5f6332f81956a074c5dbce73f7fe7758d2
Robert076 <robertberes06@gmail.com>
2025-06-27 15:31:10 +0300

    nicer output

--- cmd/code-buddy/main.go
+++ cmd/code-buddy/main.go
@@ -12,8 +12,15 @@ func main() {
 
 	reg.InitRegistry()
 
-	err := reg.RunCommand(os.Args[1], os.Args[2:])
-	if err != nil {
-		fmt.Printf("Error running command: %v", err)
+	if len(os.Args) == 1 {
+		fmt.Printf(`=====================================================
+Welcome to code-buddy. Your CLI friend.
+Example command: 'code-buddy lint go'
+=====================================================`)
+	} else {
+		err := reg.RunCommand(os.Args[1], os.Args[2:])
+		if err != nil {
+			fmt.Printf("Error running command: %v", err)
+		}
 	}
 }
