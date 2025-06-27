1e64d36ed7835f4c75db17510c19f7a1bed5121d
Robert076 <robertberes06@gmail.com>
2025-06-27 16:05:19 +0300

    gitignore command

---
+++ internal/command/gitignore_command/gitignore_command.go
@@ -0,0 +1,41 @@
+package gitignore_command
+
+import (
+	"fmt"
+
+	"github.com/Robert076/code-buddy/internal/command"
+)
+
+type GitignoreCommand struct {
+}
+
+func (g *GitignoreCommand) Name() string {
+	return "gitignore"
+}
+
+func (g *GitignoreCommand) Description() string {
+	return "Returns gitignore templates from the official Github documentation"
+}
+
+func (g *GitignoreCommand) Run(args []string) error {
+	if len(args) == 0 {
+		fmt.Println("Usage: do gitignore <language>")
+		fmt.Println("Supported gitignore templates:")
+		for _, sub := range g.Subcommands() {
+			fmt.Printf("  %s - %s\n", sub.Name(), sub.Description())
+		}
+		return nil
+	}
+
+	for _, sub := range g.Subcommands() {
+		if sub.Name() == args[0] {
+			return sub.Run(args[1:])
+		}
+	}
+
+	return fmt.Errorf("unknown gitignore template: %s", args[0])
+}
+
+func (g *GitignoreCommand) Subcommands() []command.Command {
+	return nil
+}
