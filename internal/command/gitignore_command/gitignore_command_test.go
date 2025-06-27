698c05eb8a0f6b4950c751f8c798bd3bd21d6b42
Robert076 <robertberes06@gmail.com>
2025-06-27 16:10:12 +0300

    some small tests

---
+++ internal/command/gitignore_command/gitignore_command_test.go
@@ -0,0 +1,19 @@
+package gitignore_command
+
+import "testing"
+
+func TestGitignoreCommand_Name(t *testing.T) {
+	cmd := &GitignoreCommand{}
+	want := "gitignore"
+	if got := cmd.Name(); got != want {
+		t.Fatalf("GitignoreCommand.Name() = %v, want %v", got, want)
+	}
+}
+
+func TestGitignoreCommand_Description(t *testing.T) {
+	cmd := &GitignoreCommand{}
+	want := "Returns gitignore templates from the official Github documentation"
+	if got := cmd.Description(); got != want {
+		t.Fatalf("GitignoreCommand.Description() = %v, want %v", got, want)
+	}
+}
