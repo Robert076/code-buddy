ad3e8314ce6807792a64a112a135d3087a10377c
Robert076 <robertberes06@gmail.com>
2025-06-28 08:17:38 +0300

    new tests for capture output removed redundant api call

--- internal/utils/capture_output/capture_output_test.go
+++ internal/utils/capture_output/capture_output_test.go
@@ -1,18 +1,16 @@
 package capture_output
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/Robert076/code-buddy/internal/command/gitignore_command/gitignore_go"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCaptureOutput(t *testing.T) {
-	cmd := &gitignore_go.GitignoreCommandGo{}
 	output := CaptureOutput(func() {
-		cmd.Run([]string{})
+		fmt.Print("Hello world")
 	})
-	assert.Contains(t, output, ".env")
-	assert.Contains(t, output, "# env file")
-	assert.Contains(t, output, ".exe")
+	assert.Contains(t, output, "Hello")
+	assert.Contains(t, output, "world")
 }
