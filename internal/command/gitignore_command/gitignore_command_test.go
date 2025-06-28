ab54a1b6faca0411c8ba7220dc4dd4bf2554aeac
Robert076 <robertberes06@gmail.com>
2025-06-28 11:26:37 +0300

    more tests

--- internal/command/gitignore_command/gitignore_command_test.go
+++ internal/command/gitignore_command/gitignore_command_test.go
@@ -70,3 +70,67 @@ func TestGitignoreCommand_Subcommands(t *testing.T) {
 		t.Errorf("Expected %d subcommands, got %d", 155, len(subcommands))
 	}
 }
+
+func TestGitignoreSubcommand_Name(t *testing.T) {
+	cmd := &GitignoreSubcommand{"Go"}
+	want := "Go"
+
+	if got := cmd.Name(); got != want {
+		t.Fatalf("GitignoreSubcommand_Name() got %s, want %s", got, want)
+	}
+}
+
+func TestGitignoreSubcommand_Description(t *testing.T) {
+	cmd := &GitignoreSubcommand{"Go"}
+	want := "Returns a gitignore template for Go projects"
+
+	if got := cmd.Description(); got != want {
+		t.Fatalf("GitignoreSubcommand_Description() got %s, want %s", got, want)
+	}
+}
+
+func TestGitignoreSubcommand_Subcommands(t *testing.T) {
+	cmd := &GitignoreSubcommand{"Go"}
+	subcommands := cmd.Subcommands()
+
+	if len(subcommands) != 0 {
+		t.Fatalf("Expected length of subcommands 0, got: %d", len(subcommands))
+	}
+}
+
+func TestGitignoreSubcommand_Run(t *testing.T) {
+	tests := []struct {
+		name        string
+		cmd         *GitignoreSubcommand
+		expectErr   bool
+		errMsg      string
+		description string
+	}{
+		{
+			"Valid - integration test, tests external endpoint",
+			&GitignoreSubcommand{"this-gitignore-template-doesnt-exist"},
+			true,
+			"bad return code fetching github repo for this-gitignore-template-doesnt-exist gitignore: 404",
+			"User requests a gitignore for an unknown template by running 'gitignore this-gitignore-template-doesnt-exist', this will cause a 404 return code (not found)",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.cmd.Run([]string{})
+
+			if tt.expectErr {
+				if err == nil {
+					t.Fatalf("Expected error, got nil")
+				}
+				if err.Error() != tt.errMsg {
+					t.Fatalf("Unexpected error: got %s, wanted %s", err, tt.errMsg)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Expected no error, got '%v'", err)
+				}
+			}
+		})
+	}
+}
